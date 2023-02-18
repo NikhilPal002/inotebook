ee425b02c2b530d8f2a2c27caf5dc5d87ac816c4
Nikhil <nikhilpal2017@gmail.com>
2023-02-18 16:35:47 +0530

    Created routes for storing notes and authantication

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,10 @@ connectToMongo();
 const app = express()
 const port = 5000
 
-// app.use(express.json())
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-app.get('/api/v1/login', (req, res) => {
-    res.send('Hello login!')
-  })
-app.get('/api/v1/signup', (req, res) => {
-    res.send('Hello signup!')
-  })
-// // Available routes
-// app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/notes', require('./routes/notes'))
+
+// Available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
