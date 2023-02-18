7887a1d7b9afa4d83405d3b9722eff499a8db00c
Nikhil <nikhilpal2017@gmail.com>
2023-02-18 16:23:20 +0530

    Created model and added notes and user schema

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,12 @@ const port = 5000
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+app.get('/api/v1/login', (req, res) => {
+    res.send('Hello login!')
+  })
+app.get('/api/v1/signup', (req, res) => {
+    res.send('Hello signup!')
+  })
 // // Available routes
 // app.use('/api/auth', require('./routes/auth'))
 // app.use('/api/notes', require('./routes/notes'))
