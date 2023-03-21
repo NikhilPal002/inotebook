8a75efc5f742563e45e83829d61d473a3d322553
Nikhil <nikhilpal2017@gmail.com>
2023-03-21 21:46:53 +0530

    fixed the error which was happining due to connection of localhost gone back to previous code

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017"
-// const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&directConnection=true&tls=false"
+// const mongoURI = "mongodb+srv://nikhilpal002:nikhilpal@cluster1.rxwyksh.mongodb.net/?retryWrites=true&w=majority"    atlas to app direct
+// const mongoURI = " mongodb+srv://nikhilpal002:nikhilpal@cluster1.rxwyksh.mongodb.net/test"    cluster connected to compass
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook?readPreference=primary&directConnection=true&tls=false"
 
-const connectToMongo = ()=>{         // using async and await function so that promises can resolve
-    mongoose.connect(mongoURI, ()=>{     // ()+> callback function jo ki humko kuch return karke dega 
+
+const connectToMongo = () => {         // using async and await function so that promises can resolve
+    mongoose.connect(mongoURI, () => {     // ()+> callback function jo ki humko kuch return karke dega 
         console.log("Connected to mongo successfully");
-    })
-}
+    },6000)
 
+    
+}
 module.exports = connectToMongo;
+
+
+// mongoose.connect(
+//     process.env.MONGO_URL,
+//     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+//     () => {
+//         console.log('Connected to MongoDB');
+//     }
+// );
+
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const connectToMongo = require('./db');
 const express = require('express')
-
 connectToMongo();
 const app = express()
 const port = 5000
 
-
+app.use(express.json())             // agar aap use karna chate hai body ko toh ek middle ware use karna padega
 // Available routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
@@ -13,3 +12,5 @@ app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
+
+
