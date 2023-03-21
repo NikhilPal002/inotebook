67c9b3d62d9b43fb21d1487d7bfcb28b8048a284
Nikhil <nikhilpal2017@gmail.com>
2023-03-21 22:27:10 +0530

    added security for password usinf bcryptjs and jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'Nikhilisagoodb$oy';
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
@@ -21,13 +24,25 @@ router.post('/createuser', [
     if (user) {
       return res.status(400).json({ error: "Sorry a user with this email already exists" })
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password,salt);   // await kisya hai q ki yeh promise return karta hai 
+
     // Create a new user
     user = await User.create({
       name: req.body.name,
-      password: req.body.password,
+      password: secPass,
       email: req.body.email,
     })
-    res.json(user)
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    
+    const authtoken = jwt.sign(data, JWT_SECRET);
+
+    res.json({authtoken})
     
   } catch (error) {
     console.error(error.message);
