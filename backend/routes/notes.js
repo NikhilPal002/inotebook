b7d62697dde282710ca843448fb1242c544689d2
Nikhil <nikhilpal2017@gmail.com>
2023-03-22 10:47:05 +0530

    Created a update endpoint and  add note endpoint also changed changed the notes..js to note.js

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
+// ROUTE 1: Get All the Notes using: GET "/api/notes/getuser". Login required
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes)
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
 
-    res.json([])
+// ROUTE 2: Add a new Note using: POST "/api/notes/addnote". Login required
+router.post('/addnote', fetchuser, [
+  body('title', 'Enter a valid title').isLength({ min: 3 }),
+  body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
+    try {
+
+      const { title, description, tag } = req.body;        //destructuring method
+
+      // If there are errors, return Bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {         // agar kuch error hai toh woh error ko send kardo response me 
+        return res.status(400).json({ errors: errors.array() });    // agar yeh error pass ho jaye toh ek naya note create kardo
+      }
+      const note = new Note({
+        title, description, tag, user: req.user.id
+      })
+      const savedNote = await note.save()
+
+      res.json(savedNote)
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
   })
 
-  module.exports = router
+// ROUTE 3: Update an existing Note using: POST "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  // Create a newNote object
+  const newNote = {};
+  if (title) { newNote.title = title };     // title aa raha hai as part of request to iss object me add kardo wrna nahi aa raha hai toh user update nahi karna chata hai
+  if (description) { newNote.description = description };
+  if (tag) { newNote.tag = tag };
+
+  // Find the note to be updated and update it
+  let note = await Note.findById(req.params.id);
+  if (!note) { return res.status(404).send("Not Found") }
+
+  if (note.user.toString() !== req.user.id) {       // yeh tostring dega iss note ki id
+    return res.status(401).send("Not Allowed");
+  }
+
+  note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+  res.json({ note });
+
+})
+
+module.exports = router
