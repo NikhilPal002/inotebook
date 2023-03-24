d8dc5cb94fb92ea3f88cd91dd617e67820a5934f
Nikhil <nikhilpal2017@gmail.com>
2023-03-24 11:09:42 +0530

    Created card view to show the notes item using usestate method

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react'
-import noteContext from '../context/notes/noteContext'
+import Notes from './Notes';
 
 export const Home = () => {
-  const context = useContext(noteContext);
-  const {notes, setNotes} = context
+  
   return (
     <div>
       <div className="conatiner my-3">
@@ -25,12 +24,7 @@ export const Home = () => {
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
-      <div className="conatiner my-3">
-        <h1>Your Notes</h1>
-        {notes.map((note)=>{
-            return note.title
-        })}
-      </div>
+      <Notes/>
     </div>
   )
 }
