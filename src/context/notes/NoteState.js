ee4f1733c6b61cd8f3803ca2b50f5904b2d691d0
Nikhil <nikhilpal2017@gmail.com>
2023-03-24 10:36:59 +0530

    used context to see the title of the notes on home page

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,23 +3,39 @@ import NoteContext from "./noteContext";
 
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name": "Nikhil",
-        "class": "6b"
-    }
-    
-   const [state, setState] = useState(s1);
-   const update = ()=>{
-    setTimeout(()=>{
-        setState({
-            "name": "larry",
-            "class": "10b"
-        })
-    },1000)
-   }
+    const notesInitials = [
+        {
+          "_id": "641a88104a8a8a8788f6af77",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.106Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af79",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.947Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88124a8a8a8788f6af7f",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:10.681Z",
+          "__v": 0
+        }
+      ]
+      const [notes, setNotes] = useState(notesInitials)
 
     return(
-       <NoteContext.Provider value={{state, update}}>
+       <NoteContext.Provider value={{notes, setNotes}}>
         {props.children}
        </NoteContext.Provider>
     )
