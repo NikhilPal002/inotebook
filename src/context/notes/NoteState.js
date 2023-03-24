c086526a11d3dd92d21345f8eee704211ab14050
Nikhil <nikhilpal2017@gmail.com>
2023-03-24 11:10:43 +0530

    created noteitem and notes to chow card

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -30,6 +30,87 @@ const NoteState = (props)=>{
           "tag": "personal devlop",
           "date": "2023-03-22T04:46:10.681Z",
           "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af77",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.106Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af79",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.947Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af77",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.106Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af79",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.947Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af77",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.106Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af79",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.947Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af77",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.106Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af79",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.947Z",
+          "__v": 0
+        },
+        {
+          "_id": "641a88104a8a8a8788f6af79",
+          "user": "6419eacd0bfb90953f22b931",
+          "title": "My title3",
+          "description": "wake up early in morning",
+          "tag": "personal devlop",
+          "date": "2023-03-22T04:46:08.947Z",
+          "__v": 0
         }
       ]
       const [notes, setNotes] = useState(notesInitials)
