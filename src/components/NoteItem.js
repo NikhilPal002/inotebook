f15a59a77697152d8c7889ffab580e8b4c66fe44
Nikhil <nikhilpal2017@gmail.com>
2023-03-30 10:07:15 +0530

    Added fontawasome icons

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function NoteItem(props) {
     const { note } = props;          // distructuring method
     return (
         <div className='col-md-3'>
-            <div class="card my-3">
-                <div class="card-body">
-                    <h5 class="card-title">{ }note.title</h5>
-                    <p class="card-text"> {note.description} </p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div className="card my-3">
+                <div className="card-body">
+                    <div className='d-flex align-item-center'>
+                        <h5 className="card-title">{ }note.title</h5>
+                        <i className="fa-solid fa-trash-can mx-2"></i>
+                        <i className="fa-solid fa-pen-to-square mx-2"></i>
+                    </div>
+                    <p className="card-text"> {note.description} </p>
+                    <Link to="#" className="btn btn-primary my-2">Go somewhere</Link>
                 </div>
             </div>
         </div>
