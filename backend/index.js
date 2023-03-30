56c7788ed1eebbcb9e68f6bd50926aea94fc7f6b
Nikhil <nikhilpal2017@gmail.com>
2023-03-30 11:40:58 +0530

    used npm cors package to request api from browser

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const connectToMongo = require('./db');
 const express = require('express')
+var cors = require('cors')
+
 connectToMongo();
 const app = express()
 const port = 5000
 
+app.use(cors())
 app.use(express.json())             // agar aap use karna chate hai body ko toh ek middle ware use karna padega
 // Available routes
 app.use('/api/auth', require('./routes/auth'))
