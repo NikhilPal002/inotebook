1a331cf455ada534ff1f8cae9985a6ecdc86b0f8
Nikhil <nikhilpal2017@gmail.com>
2023-03-30 10:07:30 +0530

    fixed errors

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -6,7 +6,7 @@ const NoteState = (props)=>{
     const notesInitials = [
         {
           "_id": "641a88104a8a8a8788f6af77",
-          "user": "6419eacd0bfb90953f22b931",
+          "user": "6419eacd0bfwb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
           "tag": "personal devlop",
@@ -14,7 +14,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af79",
+          "_id": "641a88104a8ae8a8788f6af79",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -23,7 +23,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88124a8a8a8788f6af7f",
+          "_id": "641aw88124a8a8a8788f6af7f",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -32,7 +32,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af77",
+          "_id": "641a88104a8a8a8788rf6af77",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -41,7 +41,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af79",
+          "_id": "641a8810w4a8a8a8788f6af79",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -50,7 +50,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af77",
+          "_id": "641a88104a8a8a8t788f6af77",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -59,7 +59,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af79",
+          "_id": "641a88104a81a8a8788f6af79",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -68,7 +68,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af77",
+          "_id": "641a88104a8a8aw8788f6af77",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -77,7 +77,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af79",
+          "_id": "641a88104a8a8a8q788f6af79",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -86,7 +86,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af77",
+          "_id": "641a88104a8sa8a8788f6af77",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -95,7 +95,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af79",
+          "_id": "641a881a04a8a8a8788f6af79",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
@@ -104,7 +104,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "641a88104a8a8a8788f6af79",
+          "_id": "641a88104a8aa8a8788f6af79",
           "user": "6419eacd0bfb90953f22b931",
           "title": "My title3",
           "description": "wake up early in morning",
