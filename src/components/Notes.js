e2dd77ce98af5dcdffdc87fa09f9e6069719e261
Nikhil <nikhilpal2017@gmail.com>
2023-04-05 21:53:31 +0530

    used bootstrap model to update the note

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,22 +1,79 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { useEffect } from 'react';
 import noteContext from '../context/notes/noteContext'
 import AddNote from './AddNote';
 import NoteItem from './NoteItem';
 
-const Notes =()=> {
+const Notes = () => {
     const context = useContext(noteContext);
     const { notes, getNotes } = context;
     useEffect(() => {
         getNotes()
+        // eslint-disable-next-line
     }, [])
+    const ref = useRef(null);
+    const [note, setNote] = useState({etitle: "", edescription: "", etag: ""})
+
+    const updateNote = (currentNote) => {
+        ref.current.click();
+        setNote({etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag});
+    }
+    const handleClick = (e)=>{
+        console.log("Updating the note")
+        e.preventDefault();
+        
+    }
+
+    const onChange = (e)=>{
+        setNote({...note, [e.target.name]: e.target.value})
+    }
+
     return (
         <>
             <AddNote />
+
+            <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+            {/*  Modal */}
+            <div classname="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div classname="modal-dialog">
+                    <div classname="modal-content">
+                        <div classname="modal-header">
+                            <h1 classname="modal-title fs-5" id="exampleModalLabel">Edit Note</h1>
+                            <button type="button" classname="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div classname="modal-body">
+                            <form className="my-3">
+                                <div className="mb-3">
+                                    <label htmlFor="title" className="form-label">Title</label>
+                                    <input type="text" className="form-control" id="etitle" name="etitle" value={note.title} aria-describedby="emailHelp" onChange={onChange} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="description" className="form-label">Description</label>
+                                    <input type="text" className="form-control" id="edescription" name="edescription" value={note
+                                    .description} onChange={onChange} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="tag" className="form-label">Tag</label>
+                                    <input type="text" className="form-control" value={note.tag} id="etag" name="etag" onChange={onChange} />
+                                </div>
+
+                               
+                            </form>
+                        </div>
+                        <div classname="modal-footer">
+                            <button type="button" classname="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button onClick={handleClick} type="button" classname="btn btn-primary">Update Note</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="row my-3">
                 <h1>Your Notes</h1>
                 {notes.map((note) => {
-                    return <NoteItem key={note._id} note={note} />
+                    return <NoteItem key={note._id} updateNote={updateNote} note={note} />
                 })}
             </div>
         </>
