d529269cdf82118ce73f25f0c3fd299cdf023a21
Nikhil <nikhilpal2017@gmail.com>
2023-04-05 22:48:14 +0530

    Fixed some bugs and validation

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -17,7 +17,7 @@ const NoteState = (props) => {
       }
     });
     const json = await response.json()
-    console.log(json)
+    
     setNotes(json)
   }
 
@@ -34,19 +34,7 @@ const NoteState = (props) => {
       body: JSON.stringify({title, description, tag})
     });
      
-    const json = await response.json();
-    console.log(json);
-
-    console.log("Adding a new note")
-    const note = {
-      "_id": "61322f119553781a8ca8d0e08",
-      "user": "6131dc5e3e4037cd4734a0664",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2021-09-03T14:20:09.668Z",
-      "__v": 0
-    };
+    const note = await response.json();
     setNotes(notes.concat(note))
   }
 
@@ -62,9 +50,7 @@ const NoteState = (props) => {
       },
     });
     const json = response.json();
-    console.log(json);
-
-    console.log("Deleting the note with id" + id);
+    
     const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
   }
@@ -82,7 +68,7 @@ const NoteState = (props) => {
       body: JSON.stringify({title, description, tag})
     });
     const json = await response.json();
-    console.log(json);
+   
 
     let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
