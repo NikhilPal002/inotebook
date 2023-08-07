740f047ef05e1947eedccb2dd50a2271e996fa68
Nikhil <nikhilpal2017@gmail.com>
2023-08-08 01:01:37 +0530

    Changed the icons and fixed all the errors

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Alert(props) {
     const capitalize = (word) => {
         const lower = word.toLowerCase();
-        return lower.charAt(0).toUpperCase() + lower.slice(1);
+        return lower.charAt(0).toUpperCase() + lower.slice(1).toLowerCase();
     }
     return (
         <div className='mt fixed-top'>
